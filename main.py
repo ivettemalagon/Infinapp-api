1b2d62e62d6463b1422684dd98eb5af640b27672
ivettemalagon <71296531+ivettemalagon@users.noreply.github.com>
2020-12-22 10:18:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ api = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080", "https://infinapp-app.herokuapp.com/",
+    "http://localhost", "http://localhost:8080", "https://infinapp-app.herokuapp.com",
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
