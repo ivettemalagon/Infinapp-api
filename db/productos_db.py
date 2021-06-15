0c26252e2a45a7c0bf64aac48cd3869380f58353
ivettemalagon <71296531+ivettemalagon@users.noreply.github.com>
2021-06-15 05:47:45 -0500

    Update productos_db.py

--- db/productos_db.py
+++ db/productos_db.py
@@ -13,25 +13,25 @@ class ProductInDB(BaseModel):
 #DB ficticia
 database_productos = Dict[str, ProductInDB]
 database_productos = {
-    "Pan_Bimbo": ProductInDB(**{"nombre_producto":"Pan Bimbo",
+    "Pan": ProductInDB(**{"nombre_producto":"Pan",
                                 "cantidad":10,
                                 "precio_compra":4500,
                                 "pvp":5000,
                                 "fecha_vencimiento":"2020-12-31"
                                 }),
-    "Jabon_Ariel": ProductInDB(**{"nombre_producto":"Jabon Ariel",
+    "Jabon": ProductInDB(**{"nombre_producto":"Jabon",
                                 "cantidad":8,
                                 "precio_compra":3850,
                                 "pvp":4200,
                                 "fecha_vencimiento":"2021-03-25"
                                 }),   
-    "Margarina_Gustosita": ProductInDB(**{"nombre_producto":"Margarina Gustosita",
+    "Margarina": ProductInDB(**{"nombre_producto":"Margarina",
                                 "cantidad":5,
                                 "precio_compra":1300,
                                 "pvp":1550,
                                 "fecha_vencimiento":"2021-09-13"
                                 }), 
-    "Jugo_DelValle": ProductInDB(**{"nombre_producto":"Jugo DelValle",
+    "Jugo": ProductInDB(**{"nombre_producto":"Jugo",
                                 "cantidad":6,
                                 "precio_compra":1700,
                                 "pvp":2000,
@@ -43,7 +43,7 @@ database_productos = {
                                 "pvp":350,
                                 "fecha_vencimiento":"2020-12-18"
                                 }),
-    "Agua_Botella": ProductInDB(**{"nombre_producto":"Agua Botella",
+    "Agua": ProductInDB(**{"nombre_producto":"Agua",
                                 "cantidad": 12,
                                 "precio_compra":1450,
                                 "pvp":1600,
